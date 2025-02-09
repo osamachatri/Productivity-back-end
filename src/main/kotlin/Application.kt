72dd0c9baed98053bd3fc69a8abf0480ea20e6d1
Oussama Chatri <oussamachatri7@gmail.com>
2025-02-09 19:26:38 +0100

    Let's go!

---
+++ src/main/kotlin/Application.kt
@@ -0,0 +1,14 @@
+package com.oussama_chatri
+
+import io.ktor.server.application.*
+
+fun main(args: Array<String>) {
+    io.ktor.server.netty.EngineMain.main(args)
+}
+
+fun Application.module() {
+    configureSecurity()
+    configureSerialization()
+    configureDatabases()
+    configureRouting()
+}
