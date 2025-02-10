dcf600a2edec7c39b1a2fbc27fb57226bf19374c
Oussama Chatri <oussamachatri7@gmail.com>
2025-02-10 22:42:01 +0100

    return the notes routes

--- src/main/kotlin/Application.kt
+++ src/main/kotlin/Application.kt
@@ -3,19 +3,21 @@ package com.oussama_chatri
 import com.oussama_chatri.routes.authRoutes
 import com.oussama_chatri.routes.notesRoutes
 import com.oussama_chatri.security.configureSecurity
+import io.ktor.http.*
 import io.ktor.serialization.kotlinx.json.*
 import io.ktor.server.application.*
 import io.ktor.server.engine.*
 import io.ktor.server.netty.*
 import io.ktor.server.plugins.contentnegotiation.*
+import io.ktor.server.response.*
 import io.ktor.server.routing.*
 
-fun main(args: Array<String>) {
-    io.ktor.server.netty.EngineMain.main(emptyArray())
-    embeddedServer(Netty, port = 8080, host = "0.0.0.0",
-        module = Application::module
-        ).start(wait = true)
+fun main(args: Array<String>): Unit {
+    io.ktor.server.netty.EngineMain.main(args)
+    embeddedServer(Netty, port = 8080, host = "127.0.0.1") {
+    }.start(true)
 }
+
 
 fun Application.module() {
 
@@ -27,6 +29,9 @@ fun Application.module() {
     DatabaseFactory.db
 
     routing {
+        get("/") {
+            call.respondText("HELLO WORLD!", contentType = ContentType.Text.Plain)
+        }
         authRoutes()
         notesRoutes()
     }
