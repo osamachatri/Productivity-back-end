804fec495d525f33acc36819767d58b27fdf76f3
Oussama Chatri <oussamachatri7@gmail.com>
2025-02-11 00:12:50 +0100

    gradle changes

--- build.gradle.kts
+++ build.gradle.kts
@@ -51,3 +51,11 @@ dependencies {
     implementation("io.ktor:ktor-server-auth:2.3.6")
     implementation("io.ktor:ktor-server-auth-jwt:2.3.6")
 }
+
+tasks.jar {
+    manifest {
+        attributes(
+            "Main-Class" to "com.yourpackage.YourMainClass"
+        )
+    }
+}
